ffece36687c1cf3de0c0d53f225615aa92cffec5
Saglara <nohaevass@yandex.ru>
2022-09-20 20:25:19 +0300

    Случайным образом из числа в пределах от 10 до 99 выбирается наибольшая цифра

---
+++ Ex1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int randomNum=new Random().Next(10,100); //74
+Console.WriteLine($"Число {randomNum} ");
+int a1=randomNum/10; //7
+int a2=randomNum%10; //4
+int max=a1;
+if (a2>max) max=a2;
+Console.WriteLine($"Максимальное цифра {max} в числе {randomNum} ");
